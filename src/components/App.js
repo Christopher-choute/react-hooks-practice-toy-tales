a85e990956ba1fbeac14b6b6d85d365c303ba4dc
Chris Choute <christopherchoute@gmail.com>
2023-02-05 17:09:54 -0500

    killing it

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
 function App() {
+
+  useEffect(() => {
+    fetch('http://localhost:3001/toys')
+    .then(response => response.json())
+    .then(data => setToyArray(data))
+  }, []);
+
+  const [toyArray, setToyArray] = useState([]);
   const [showForm, setShowForm] = useState(false);
 
   function handleClick() {
@@ -18,7 +26,7 @@ function App() {
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toyArray = {toyArray} />
     </>
   );
 }
